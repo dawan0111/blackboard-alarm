8d8906829ca0a68bd9cdc8070e7f1b9016b2034f
KDW <msd030428@gmail.com>
2020-09-22 11:15:47 +0900

    공식문서 하란대로 수정;;

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 import os
 
@@ -127,3 +128,5 @@ USE_TZ = True
     
         
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
